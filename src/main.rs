c770e879d1800105bb9d3c71dc65ea1ab6bbaa0e
Malcolm Seyd <malcolm@seyd.ca>
2024-07-11 02:31:48 -0700

    Add basic image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rand::prelude::*;
+use rayon::prelude::*;
+
 use std::{
     fs::File,
     io::{stdin, stdout, Cursor, Read, Write},
@@ -5,7 +8,8 @@ use std::{
 
 use anyhow::{Context, Ok, Result};
 use clap::Parser;
-use image::{io::Reader as ImageReader, RgbaImage};
+use image::{io::Reader as ImageReader, Rgba};
+use tiny_skia::{IntSize, Pixmap, PixmapRef};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -40,55 +44,143 @@ fn main() -> Result<()> {
         .decode()?
         .into_rgba8();
 
-    let output_buf = run(img)?;
+    let pixmap = {
+        let size = IntSize::from_wh(img.width(), img.height())
+            .context("failed to construct input pixbuf size")?;
+        Pixmap::from_vec(img.into_vec(), size).context("failed to construct input pixbuf")?
+    };
+
+    let output_buf = run(pixmap.as_ref())?;
     output.write_all(&output_buf)?;
 
     Ok(())
 }
 
-const GENERATIONS: u64 = 1000;
+const GENERATIONS: usize = 1000;
+const GENERATION_SIZE: usize = 1000;
+const GENERATION_PARENTS: usize = GENERATION_SIZE * 5 / 10;
 
 enum Shape {
-    /// Circle centered on cx and cy
-    Circle { cx: f32, cy: f32, radius: f32 },
+    /// Circle centered on `cx` and `cy` with radius `r`
+    Circle {
+        cx: f32,
+        cy: f32,
+        r: f32,
+        color: Rgba<u8>,
+    },
+}
+
+impl Shape {
+    fn to_svg(&self, w: &mut impl Write) -> Result<()> {
+        match self {
+            Self::Circle { cx, cy, r, color } => {
+                write!(
+                    w,
+                    r#"<circle cx="{cx}" cy="{cy}" r="{r}" fill="rgb({} {} {})"/>"#,
+                    color.0[0], color.0[1], color.0[2]
+                )
+            }
+        }?;
+        Ok(())
+    }
 }
 
 struct Gene {
     shapes: Vec<Shape>,
 }
 
+impl Gene {
+    fn to_svg(&self, input: PixmapRef, w: &mut impl Write) -> Result<()> {
+        write!(
+            w,
+            r#"<svg viewBox="0 0 {} {}" xmlns="http://www.w3.org/2000/svg" width="{}" height="{}">"#,
+            input.width(),
+            input.height(),
+            input.width(),
+            input.height()
+        )?;
+        for shape in self.shapes.iter() {
+            shape.to_svg(w)?
+        }
+        write!(w, r#"</svg>"#)?;
+        Ok(())
+    }
+}
+
 type Population = Vec<Gene>;
 
-fn run(input: RgbaImage) -> Result<Vec<u8>> {
+fn run(input: PixmapRef) -> Result<Vec<u8>> {
     // TODO: use tiny_skia to draw circles of random color and location onto a Pixmap
     // TODO: use MSE to calculate total error
     let mut prev: Population = init_generation();
+    let mut rng = StdRng::from_entropy();
 
     for _ in 0..GENERATIONS {
-        let parents = select_parents(prev);
-        let mut current = crossover_genes(parents);
-        mutate_genes(&mut current);
+        let parents = select_parents(input, prev);
+        let mut current = crossover_genes(&mut rng, parents);
+        mutate_genes(&mut rng, input, &mut current);
         prev = current;
     }
 
-    let output = Vec::new();
+    // TODO: properly select the best for output
+    let best = &prev[0];
+
+    let mut output = Vec::new();
+    best.to_svg(input, &mut output)?;
+
     Ok(output)
 }
 
 fn init_generation() -> Population {
-    todo!()
+    let mut init: Population = Vec::new();
+
+    for _ in 0..GENERATION_SIZE {
+        let shapes = Vec::new();
+        init.push(Gene { shapes });
+    }
+
+    init
 }
 
-fn select_parents(prev: Population) -> Population {
-    // TODO: evaluate fitness of parents and prune the worst
-    prev
+fn select_parents(input: PixmapRef, prev: Population) -> Population {
+    let mut results = prev
+        .into_par_iter()
+        .map(|gene| (evaluate(input, &gene), gene))
+        .collect::<Vec<_>>();
+
+    results.sort_by(|(score_a, _), (score_b, _)| score_a.total_cmp(score_b));
+
+    // TODO: prune the worst
+
+    results.into_iter().map(|(_, gene)| gene).collect()
 }
 
-fn crossover_genes(parents: Population) -> Population {
-    // TODO: share objects from parents randomly
+fn evaluate(input: PixmapRef, gene: &Gene) -> f32 {
+    // TODO: calculate error on pixels
+    0.0
+}
+
+fn crossover_genes(rng: &mut impl Rng, parents: Population) -> Population {
+    // TODO: share genes from parents randomly
     parents
 }
 
-fn mutate_genes(current: &mut Population) {
-    // TODO: mutate the new generation randomly
+fn mutate_genes(rng: &mut impl Rng, input: PixmapRef, current: &mut Population) {
+    let w = input.width() as f32;
+    let h = input.height() as f32;
+    for p in current.iter_mut() {
+        // add shape
+        if rng.gen_ratio(1, 10) {
+            // add circle
+            let color: Rgba<u8> = rng.gen::<[u8; 4]>().into();
+
+            let r = rng.gen_range(0.0..=(w.max(h)));
+            p.shapes.push(Shape::Circle {
+                cx: rng.gen_range(-r..w + r),
+                cy: rng.gen_range(-r..h + r),
+                r,
+                color,
+            })
+        }
+    }
 }
