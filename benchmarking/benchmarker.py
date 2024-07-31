1fbe7660a31f0d02e90e86e768e2aca228c2c882
JulianF <julian123france@gmail.com>
2024-07-30 20:01:37 -0700

    Plotting every sample from gen output

--- benchmarking/benchmarker.py
+++ benchmarking/benchmarker.py
@@ -6,29 +6,43 @@ from matplotlib.ticker import MaxNLocator
 
 # Paths
 executable = "../target/release/r2v"
-discord_small = "../datasets/discord_small.jpg"
+discord_small = "../datasets/uvic-discord_small.jpg"
+uvic_logo = "../datasets/uvic-logo.jpg"
 
 # General variables
 current_time = time.time()
-current_file_name = "discord_small.jpg"
-current_file_path = discord_small
+current_file_name = "uvic-logo.jpg"
+current_file_path = uvic_logo
 current_parameter = "g"
 
-default_generations = 1000
+default_generations = 500
 default_size = 1000
 default_time = 30
 default_elitism = 0.05
 
 run_results = []
 score_samples = []
+sample_times = []
 benchmark_runs = []
-sample_times = [0, 5, 10, 15, 20, 25]
 
 # Benchmark variables
 generations_range = [250, 500, 750, 1000, 1250]
-# size_range = [500, 1000, 1500, 2000, 2500]
-size_range = [500, 1000]
+size_range = [500, 1000, 1500, 2000, 2500]
 elitism_range = [0.01, 0.05, 0.1, 0.15, 0.2]
+line_colours = ['b', 'r', 'g', 'y', 'm']
+
+# Create benchmark to organise results
+class Benchmark:
+    def __init__(self):
+        self.size_range = size_range
+        self.elitism_range = elitism_range
+        self.sample_times = sample_times
+        self.benchmark_runs = benchmark_runs
+        self.line_colours = line_colours
+        self.labels = []
+
+size_benchmark = Benchmark()
+size_benchmark.labels = ["500", "1000", "1500", "2000", "2500"]
 
 # Get parameters to run as input
 do_generations = None
@@ -70,7 +84,7 @@ if do_generations:
         current_time = time.time()
 
         # Run the r2v program with the specified image, adjusting the selected parameter over the specified range
-        benchmark = subprocess.Popen([executable, "--outfile", "gen_output" + str(index) + ".svg", current_file_path, "-g", str(generations), "-s", str(default_size), "-p", str(default_elitism), "-t", str(default_time)], stdout=subprocess.PIPE)
+        benchmark = subprocess.Popen([executable, "--outfile", "gen_output" + str(index) + ".svg", current_file_path, "-g", str(generations), "-s", str(default_size), "-p", str(default_elitism), "-t", "600"], stdout=subprocess.PIPE)
 
         # Await the completion of the benchmark
         print("Benchmark started on parameter " + current_parameter + " = " + str(generations) + " (" + current_file_name + "): 0.00 seconds elapsed")
@@ -95,33 +109,53 @@ if do_size:
         # Await the completion of the benchmark
         print("Benchmark started on parameter " + current_parameter + " = " + str(size) + " (" + current_file_name + "): 0.00 seconds elapsed")
         while benchmark.poll() is None:
-            # Stream the output of the benchmark
-            # score_samples.append(benchmark.stdout.readlines()[-1].decode("utf-8").split(" ")[-1].strip())
-            # print(benchmark.stdout.readlines()[-1])
             time.sleep(5)
             print("Benchmark running on parameter " + current_parameter + " = " + str(size) + " (" + current_file_name + "): {:.2f} seconds elapsed".format(time.time() - current_time))
 
         # Extract the results of the benchmark
         stdout = benchmark.stdout.readlines()
 
-        sample_timer = 0.1
+        sample_delay = 1
+        eval_line = ""
         score_samples = []
+        sample_times_list = []
 
-        # Loop through lines until a line starts with "Elapsed time:" > 5 seconds
+        # Loop through lines
         for line in stdout:
             if line.decode("utf-8").startswith("Elapsed time:"):
-                if float(line.decode("utf-8").split(" ")[2].strip()) > sample_timer:
-                    sample_timer += 1
-                    new_sample = line.decode("utf-8").split(" ")[6].strip()
-                    if new_sample == "0.00":
-                        new_sample = line.decode("utf-8").split(" ")[8].strip()
+                sample_time = float(line.decode("utf-8").split(" ")[2].strip())
+                if float(sample_time > sample_delay):
+                    new_sample = eval_line.decode("utf-8").split(" ")[1].strip()
+                    sample_times_list.append(sample_time)
                     score_samples.append(int(float(new_sample)))
-
-        print(score_samples)
+            else:
+                eval_line = line
+                
+        # print(score_samples)
         benchmark_runs.append(score_samples)
-        # final_line = stdout[-1]
-        # final_score = final_line.decode("utf-8").split(" ")[-1].strip()
-        # run_results.append(final_score)
+        sample_times.append(sample_times_list)
+
+    # Create line graph of the benchmark results
+    plot.figure(figsize=(8, 6))
+
+    for i in range(len(benchmark_runs)):
+        plot.plot(sample_times[i], benchmark_runs[i], linestyle='-', color=line_colours[i], label=size_benchmark.labels[i])
+
+    # Add title and labels
+    plot.title('Generation Size Benchmark')
+    plot.xlabel('Time (s)')
+    plot.ylabel('Mean-Squared Error')
+
+    # Add grid
+    plot.grid(True)
+
+    # Add legend
+    plot.legend()
+
+    # Save the plot
+    plot.savefig('size_benchmark.png', format='png')
+    plot.show()
+
 
 
 if do_elitism:
@@ -143,36 +177,3 @@ if do_elitism:
             print("Benchmark running on parameter " + current_parameter + " = " + str(generations) + " (" + current_file_name + "): {:.2f} seconds elapsed".format(time.time() - current_time))
 
         # Extract the results of the benchmark
-
-
-# Create line graph of the benchmark results
-plot.figure(figsize=(8, 6))
-# plot.plot(sample_times, benchmark_runs[0], marker='o', linestyle='-', color='b')
-# plot.plot(sample_times, benchmark_runs[1], marker='o', linestyle='-', color='r')
-# plot.ylim(0, None)
-plot.yticks([0, 100, 300, 600, 1500, 5000, 12500, 25000, 50000])
-# Set y axis distribution to ~logarithmic
-
-# plot.yscale("log")
-# plot.gca().invert_yaxis()
-plot.plot(sample_times, benchmark_runs[0], linestyle='-', color='b')
-plot.plot(sample_times, benchmark_runs[1], linestyle='-', color='r')
-# plot.gca().set_yticklabels(1-plot.gca().get_yticks())
-
-# plot.plot(sample_times, score_samples, marker='o', linestyle='-', color='r', label='y = x^2')
-
-# Add title and labels
-plot.title('Generation Size Benchmark')
-plot.xlabel('Time (s)')
-plot.ylabel('Mean-Squared Error')
-
-# Add grid
-plot.grid(True)
-
-# Add legend
-plot.legend()
-
-# Save the plot
-plot.savefig('size_benchmark.png', format='png')
-
-plot.show()
